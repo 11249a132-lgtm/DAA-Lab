8fb7ee99f909e3dccd8a0fa5f1f8f053370dd271
11249a132-lgtm <11249a132@kanchiuniv.ac.in>
2026-04-27 20:48:14 +0530

    Create Implement Tower of Hanoi.c

---
+++ Implement Tower of Hanoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void toh(int disk_count, char S, char D, char T)
+{
+//check base condition
+if (disk_count== 1)
+{
+printf("\nMove disk %d from %c to %c",disk_count,S,D);
+return;
+}
+//move top(n-1) disks to auxillary rod 
+toh(disk_count-1,S,T,D);
+printf("\nMove disk %d from %c to %c",disk_count,S,D);
+//move (n-1) disks from auxillary to destination  
+toh(disk_count-1,T,D,S);
+}
+int main()
+{
+int n; 
+  //no.of disks
+printf("enter number of disks: ");
+scanf("%d", &n);
+  //function call
+toh(n,'S','D','T');
+return 0;
+}
